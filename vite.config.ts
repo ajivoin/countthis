281e2be93f7da8a9794b47fd30a2056c44764aba
Andrew Jivoin <andrew@jivoin.com>
2023-11-13 02:29:07 -0500

    yep

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/countthis/',
 	plugins: [
 		sveltekit(),
-		// SvelteKitPWA(),
 	],
-	build: {
-		outDir: "/dist"
-	}
 });
